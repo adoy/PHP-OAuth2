25bbe6df2c00942af754ed539784547e81fe01b0
Pierrick Charron <pierrick@webstart.fr>
2011-10-26 00:29:27 -0400

    Add https handling with certificate

--- Client.php
+++ Client.php
@@ -122,13 +122,21 @@ class Client
     protected $access_token_param_name = 'access_token';
 
     /**
+     * The path to the certificate file to use for https connections
+     * 
+     * @var string  Defaults to . 
+     */
+    protected $certificate_file = null;
+
+    /**
      * Construct 
      * 
      * @param string $client_id Client ID
      * @param string $client_secret Client Secret
      * @param int    $client_auth (AUTH_TYPE_URI, AUTH_TYPE_AUTHORIZATION_BASIC, AUTH_TYPE_FORM)
+     * @param string $certificate_file Indicates if we want to use a certificate file to trust the server. Optional, defaults to null.
      */
-    public function __construct($client_id, $client_secret, $client_auth = self::AUTH_TYPE_URI)
+    public function __construct($client_id, $client_secret, $client_auth = self::AUTH_TYPE_URI, $certificate_file = null)
     {
         if (!extension_loaded('curl')) {
             throw new \Exception('The PHP exention curl must be installed to use this library.');
@@ -137,6 +145,10 @@ class Client
         $this->client_id     = $client_id;
         $this->client_secret = $client_secret;
         $this->client_auth   = $client_auth;
+        $this->certificate_file = $certificate_file;
+        if (!empty($this->certificate_file)  && !is_file($this->certificate_file)) {
+            throw new \Exception('The certificate file was not found');
+        }
     }
 
     /**
@@ -407,12 +419,28 @@ class Client
 
         $ch = curl_init();
         curl_setopt_array($ch, $curl_options);
+        // https handling
+        if (!empty($this->certificate_file)) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+            curl_setopt($ch, CURLOPT_CAINFO, $this->certificate_file);
+        } else {
+            // bypass ssl verification
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
         $result = curl_exec($ch);
         $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+        if ($curl_error = curl_error($ch)) {
+            return array(
+                'result' => false,
+                'error' => $curl_error
+            );
+        } else { 
+            $json_decode = json_decode($result, true);
+        }
         curl_close($ch);
-
-        $json_decode = json_decode($result, true);
+        
         return array(
                 'result' => (null === $json_decode) ? $result : $json_decode,
                 'code' => $http_code,
