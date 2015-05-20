8584f93cf52bef5a350b2d50839c2dda6f3f9ac8
Pierrick Charron <pierrick@webstart.fr>
2015-05-20 11:30:59 -0400

    Bump version

--- src/OAuth2/Client.php
+++ src/OAuth2/Client.php
@@ -22,7 +22,7 @@
  *
  * @author      Pierrick Charron <pierrick@webstart.fr>
  * @author      Anis Berejeb <anis.berejeb@gmail.com>
- * @version     1.2.0
+ * @version     1.2.1-dev
  */
 namespace OAuth2;
 
